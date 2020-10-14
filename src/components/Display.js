b4a785e582df444f66a83b55936411627a07ab19
somoye123 <somoye.ayotunde@gmail.com>
2020-10-14 07:18:29 +0100

    implement display ccomponent to accept props of type string and set it default value

---
+++ src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function({ result }) {
+  return (
+    <div className="result">{result}</div>
+  );
+}
+Display.defaultProps = {
+  result: '0',
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
