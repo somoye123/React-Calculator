51fd9e120ead37215a14861c3df29d0387b11f86
somoye123 <somoye.ayotunde@gmail.com>
2020-10-15 11:16:33 +0100

    import calulate module to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../style.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+// eslint-disable-next-line no-unused-vars
+import Calculate from '../logic/calculate';
 
 export default function App() {
   return (
