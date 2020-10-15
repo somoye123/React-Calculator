2537d0fc448921dd36a4ac923ec7da52b1b5df63
somoye123 <somoye.ayotunde@gmail.com>
2020-10-15 18:07:56 +0100

    destructure clickHandler from props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Button({ buttonName, color, wide }) {
+export default function Button({ buttonName, color, wide, clickHandler }) {
   const handleClick = buttonName => clickHandler(buttonName);
 
   return (
