f31d12077639aec6d04939ddb0d181e664fe6d19
somoye123 <somoye.ayotunde@gmail.com>
2020-10-15 10:49:20 +0100

    implement calculate.js module logic

---
+++ src/logic/calculate.js
@@ -0,0 +1,48 @@
+import operate from './operate';
+
+export default (calculator, buttonName) => {
+  const operations = ['+', '-', 'x', '÷'];
+
+  let { total, next, operation } = calculator;
+
+  if (buttonName === '+/-') {
+    if (next) {
+      next = +next * -1;
+    }
+    total = +total * -1;
+  }
+
+  if (buttonName === '%') {
+    if (next) {
+      next *= 0.01;
+    } else {
+      total *= 0.01;
+    }
+  }
+
+  if (buttonName === '=') {
+    if (next) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+  }
+
+  if (operations.includes(buttonName)) {
+    operation = buttonName;
+  }
+
+  if (buttonName === '.') {
+    if (next && !next.toString().contains('.')) {
+      next = `${next.toString()}.`;
+    }
+  }
+
+  if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  }
+
+  return { total, next, operation };
+};
